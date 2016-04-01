c06fade7c175d3d46a626dc2a5727655466e9d43
michael.craig <michael.craig@finmeccanica.com>
2016-04-01 11:44:39 +0000

    working reader

--- testWrite.c
+++ testWrite.c
@@ -4,38 +4,41 @@
 
 
 int read_raw_val() {
-	int data = 23;
-	int chip_select = 4;
-	int clock = 17;
+	int data = 22;
+	int chip_select = 17;
+	int clock = 18;
+	int pin_high = 1;
+	int pin_low = 0;
 	double tick = 0.0001;
 	int a = 0;
 	int output = 0;
 	int readbit = 0;
 
-	gpioWrite(4, 1);
+	gpioWrite(chip_select, pin_high);
 	time_sleep(tick);
-	gpioWrite(17, 1);
+	gpioWrite(clock, pin_high);
 	time_sleep(tick);
-	gpioWrite(4, 0);
+	gpioWrite(chip_select, pin_low);
 	time_sleep(tick);
-	gpioWrite(17, 0);
+	gpioWrite(clock, pin_low);
 	time_sleep(tick);
 	while (a < 16) {
-		gpioWrite(17, 1);
-		time_sleep(tick);
-		gpioWrite(23, 1);
-		readbit = gpioRead(23);
-		gpioWrite(23, 0);
+		gpioWrite(clock, 1);
+		time_sleep(0.01);
+		readbit = gpioRead(data);
 		output = (output << 1) + readbit;
-		gpioWrite(17, 0);
-		time_sleep(tick);
+		gpioWrite(clock, 0);
+		time_sleep(0.01);
 		a += 1;
 	};
 	return output;
 }
 int main(int argc, char *argv[])
 {
-   	int rawval = 0;
+	int data = 22;
+   	int chip_select = 17;
+	int clock = 18;
+	int rawval = 0;
 	double start = time_time();
 
 	if (gpioInitialise() < 0)
@@ -43,14 +46,14 @@ int main(int argc, char *argv[])
       		fprintf(stderr, "pigpio initialisation failed\n");
       		return 1;
    	}
-	gpioSetMode(17, PI_OUTPUT);
-   	gpioSetMode(23, PI_INPUT);
-	gpioSetMode(4, PI_OUTPUT);
+	gpioSetMode(chip_select, PI_OUTPUT);
+   	gpioSetMode(data, PI_INPUT);
+	gpioSetMode(clock, PI_OUTPUT);
 
 	while (1) {
 		rawval = read_raw_val();
 		printf("read: %d \n",  rawval);
 		printf("raw rotation: %d \n", (rawval >> 6));
-		time_sleep(0.005);
+		time_sleep(1);
 	};
 }
