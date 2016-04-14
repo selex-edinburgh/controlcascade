5e261541d952a147c94e76abb96d9a86d3ad3c36
michael.craig <michael.craig@finmeccanica.com>
2016-04-14 14:05:35 +0000

    revised working version

--- testRolloverPiped16.c
+++ testRolloverPiped16.c
@@ -5,7 +5,6 @@ int reverse = 1;
 struct timeval {
 	int tv_sec;
 	int tv_usec;
-
 };
 
 int read_raw() {
@@ -45,10 +44,6 @@ int read_raw() {
         };
 	data0 = ((data0 + (reverse)) % ( 1 << READING_BIT_LENGTH)) & mask;
         data1 = ((data1 - (reverse)) % ( 1 << READING_BIT_LENGTH)) & mask;
-        //print_bits(sizeof(r1), &r1);
-        //uint64_t rx = r1 << READING_1_LOW_BIT;
-        //print_bits(sizeof(rx), &rx);
-        //printf ( "r0r1 %d 5d \n\n", r0,r1);
         return (data1 << READING_LOW_1_BIT) | (data0 << READING_LOW_0_BIT);
 }
 
@@ -77,30 +72,7 @@ int* handle_rollovers( int readings[2] ) {
         return values;
 }
 
-main() {
-
-	char filename[] = "fifo.tmp";
-        struct timespec ts;
-        struct timespec ts2;
-        ts.tv_sec = 1;
-        ts.tv_nsec = 0;
-
-	int s_fifo = mkfifo(filename, S_IRWXU);
-/*        if (s_fifo != 0)
-        {
-                printf("mkfifo() error: %d\n", s_fifo);
-                return -1;
-        }
-*/
-        FILE * wfd = fopen(filename, "w");
-        if (wfd < 0)
-        {
-                printf("open() error: %d\n", wfd);
-                return -1;
-        }
-
-
-
+void gpio_setup() {
 	if (gpioInitialise() < 0)
 	{
 		fprintf(stderr, "pigpio initialisation failed\n");
@@ -112,6 +84,23 @@ main() {
         gpioSetMode(DATA_PIN_2, PI_INPUT);
         gpioSetMode(CLOCK_PIN_1, PI_OUTPUT);
         gpioSetMode(CLOCK_PIN_2, PI_OUTPUT);
+}
+
+void writer(sleepValue) {
+	char filename[] = "fifo.tmp";
+        struct timespec ts;
+        struct timespec ts2;
+        ts.tv_sec = 1;
+        ts.tv_nsec = 0;
+
+	int s_fifo = mkfifo(filename, S_IRWXU);
+
+        FILE * wfd = fopen(filename, "w");
+        if (wfd < 0)
+        {
+                printf("open() error: %d\n", wfd);
+                return -1;
+        }
 
         while (1) {
 		struct timeval tv;
@@ -130,9 +119,24 @@ main() {
 			break;
 		}
 		fflush(wfd);
-                usleep(10000);
+                usleep(sleepValue);
                 if (values[0]*reverse > 20000) reverse *= -1;
         }
-	fclose(wfd);
-	unlink(filename);
+
+}
+
+int main(int argc, char *argv[]) {
+	int sleepValue;
+	char *ptr;
+
+	gpio_setup();
+	if (argc < 2)
+	{
+		sleepValue = 1000;
+	}
+	else
+	{
+		sleepValue = strtol(argv[1], &ptr, 10);
+	}
+	writer(sleepValue);
 }
