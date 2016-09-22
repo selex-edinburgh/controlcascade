60ebc3f25c993eababb4b739ff8cfb32cd6c20bf
joshua.hessey <joshua.hessey@leonardocompany.com>
2016-09-22 15:19:22 +0000

    Added wheelBase with trackWidth to trackControl 	modified:   main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def runControlLoops():
     routeState  = routeControl.RouteState(120.0)        #RouteState(near)
     odoState    = odoControl.OdoState()    #OdoState(mmPerPulse,initTheta)
     rcChanState = rcChanControl.RcChanState(40, 40, 0.5)    #RcChanState(lrChange, fwdbkChange, speedScaling)
-    trackState  = trackControl.TrackState(155,500)      #TrackState(trackWidth,movementBudget)
+    trackState  = trackControl.TrackState(200,237,500)      #TrackState(wheelBase,trackWidth,movementBudget)
     vsimState   = vsimControl.VsimState(1.5,1.0,600.0) #VsimState(fricEffectPerSec,lrBias,speedMax)
     envSimState = envSimControl.EnvSimState()
     sensorState = sensorControl.SensorState(30,5)
